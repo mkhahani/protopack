3d282a4f994e115533abc4a8aa2b59cc961efa6e
Mohsen Khahani <mkhahani@gmail.com>
2012-08-04 17:32:41 +0430

    First commit

---
+++ input/protopack.input.js
@@ -0,0 +1,131 @@
+/**
+ *  Protopack Input, a DHTML Input Component based on Prototype JS framework
+ *  © 2011-2012 Mohsen Khahani
+ *
+ *  Licensed under the MIT license
+ *  Created on September 17, 2011
+ *
+ *  http://mohsen.khahani.com/protopack
+ */
+
+
+/**
+ * Default configuration
+ */
+var ProtopackInputOptions = {
+    className   : 'protopack-input',
+    readonly    : true,
+    buttonStyle : 'smart',  // [disabled, visible, smart]
+    popupStyle  : 'auto'    // [disabled, manually, auto]
+}
+
+/**
+ * ProtopackInput base class
+ */
+var ProtopackInput = Class.create({
+    Version: '1.0',
+
+    initialize: function (target, options) {
+        this.options     = Object.clone(ProtopackInputOptions);
+        Object.extend(this.options, options || {});
+        this.className   = this.options.className;
+        this.readonly    = this.options.readonly;
+        this.buttonStyle = this.options.buttonStyle;
+        this.popupStyle  = this.options.popupStyle;
+        this.xhtml       = this.construct();
+        if (target) {
+            try {
+                $(target).insert(this.xhtml);
+                this.render();
+            } catch (err) {
+                throw new Error('The target element was not found.');
+            }
+        }
+    },
+
+    construct: function () {
+        var xhtml  = new Element('div', {'class': this.className});
+        this.entry = this._buildEntry();
+        xhtml.insert(this.entry);
+        if (this.buttonStyle !== 'disabled') {
+            this.button = this._buildButton(xhtml);
+            xhtml.insert(this.button);
+        }
+        if (this.popupStyle !== 'disabled') {
+            this.popup = this._buildPopup();
+            xhtml.insert(this.popup);
+        }
+        return xhtml;
+    },
+
+    _buildEntry: function () {
+        var entry = new Element('input', {type: 'text'});
+        if (this.readonly) {
+            entry.writeAttribute({readonly: true});
+        }
+        if (this.buttonStyle === 'smart') {
+            entry.observe('mousedown', function () {this.button.hide()}.bind(this));
+        }
+        entry.observe('mousedown', this._onInputClick.bind(this));
+
+        return entry;
+    },
+
+    _buildButton: function (xhtml) {
+        var button = new Element('button');
+        if (this.buttonStyle === 'smart') {
+            button.hide();
+            xhtml.observe('mouseover', function () {button.show()}.bind(this));
+            xhtml.observe('mouseout', function () {button.hide()}.bind(this));
+        }
+        button.observe('click', this._onButtonClick.bind(this));
+
+        return button;
+    },
+
+    _buildPopup: function () {
+        var popup = new Element('div', {'class': this.className + '-popup'}).hide();
+        popup.observe('mouseover', function () {this.hasFocus = true}.bind(this));
+        popup.observe('mouseout', function () {this.hasFocus = false}.bind(this));
+        document.observe('click', this._onPopupLostFocus.bind(this));
+
+        return popup;
+    },
+
+    _onInputClick: function (e) {
+        if (this.popupStyle === 'auto') {
+            this.popup.toggle();
+            // Event.stop(e); // To not be editable
+        }
+    },
+
+    _onButtonClick: function (e) {
+        if (this.popupStyle === 'auto') {
+            this.popup.toggle();
+            //Event.stop(e);
+        }
+    },
+
+    _onPopupLostFocus: function (e) {
+        var el = Event.findElement(e);
+        if (el !== this.entry && !this.hasFocus) {
+            this.popup.hide();
+        }
+    },
+
+    get: function () {
+        return this.xhtml;
+    },
+
+    render: function () {
+        if (Element.getLayout()) {  // Prototype 7+
+            if (this.popup.getWidth() < this.entry.getWidth()) {
+                setEqualWidth(this.entry, this.popup);
+            }
+        }
+    },
+
+    openPopup: function () {
+        this.popup.show();
+    }
+});
---
+++ protopack.js
@@ -0,0 +1,52 @@
+/**
+ * Checks for existance of the Prototype JS
+ */
+if (typeof Prototype === 'undefined' || !Prototype.Version.match('1.7')) {
+    throw('Protopack requires the Prototype JavaScript framework 1.7.0+');
+}
+
+/**
+ * Defines Prototype.Browser.IE6 as boolean
+ */
+if (Object.isUndefined(Prototype.Browser.IE6)) {
+	Prototype.Browser.IE6 = (navigator.appName.indexOf("Microsoft Internet Explorer") != -1 && 
+                             navigator.appVersion.indexOf("MSIE 6.0") != -1 && 
+                             !window.XMLHttpRequest);
+}
+
+/**
+ * Adds innerText property to all Elements
+ */
+Element.addMethods({  
+    getInnerText: function (element) {
+        element = $(element);
+        return (element.innerText && !window.opera)? element.innerText :
+            element.innerHTML.stripScripts().unescapeHTML().replace(/[\n\r\s]+/g, ' ');
+    }
+});
+
+/**
+ * Sets width of the target element same as the source element
+ */
+function setEqualWidth(sourceEl, targetEl) {
+    var e1 = $(sourceEl),
+        e2 = $(targetEl),
+        layout = new Element.Layout(e2),
+        sourceWidth = e1.getWidth(),
+        padding = layout.get('padding-left') + layout.get('padding-right'),
+        border  = layout.get('border-left')  + layout.get('border-right');
+    e2.setStyle({width: sourceWidth - padding - border + 'px'});
+}
+
+/**
+ * Sets height of the target element same as the source element
+ */
+function setEqualHeight(sourceEl, targetEl) {
+    var e1 = $(sourceEl),
+        e2 = $(targetEl),
+        layout = new Element.Layout(e2),
+        sourceHeight = e1.getHeight(),
+        padding = layout.get('padding-top') + layout.get('padding-bottom'),
+        border  = layout.get('border-top')  + layout.get('border-bottom');
+    e2.setStyle({height: sourceHeight - padding - border + 'px'});
+}
