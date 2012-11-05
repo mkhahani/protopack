bb8fb482e8806ba65c6cbe54a7328780d5c2dc0a
Mohsen Khahani <mkhahani@gmail.com>
2012-11-05 10:39:02 +0330

    add constructor and restructure window.js

--- window.js
+++ window.js
@@ -1,11 +1,11 @@
 /**
- *  Protopack Window, a DHTML Window Component based on Prototype JS framework
- *  © 2012 Mohsen Khahani
+ * Protopack Window, a DHTML Window Component based on Prototype JS framework
+ * © 2012 Mohsen Khahani
  *
- *  Licensed under the MIT license
- *  Created on May 6, 2012
+ * Licensed under the MIT license
+ * Created on May 6, 2012
  *
- *  http://mohsen.khahani.com/protopack
+ * http://mohsen.khahani.com/protopack
  */
 
 
@@ -13,14 +13,14 @@
  * Default configuration
  */
 var ProtopackWindowOptions = {
-    className       : 'protopack-window',
+    className       : 'pwindow',
     showHeader      : true,
     closeButton     : true,
+    modal           : true,
     escape          : true,
     draggable       : true,
-    modal           : true,
     transparentDrag : true
-}
+};
 
 /**
  * ProtopackWindow class
@@ -36,36 +36,58 @@ var ProtopackWindow = Class.create({
         Object.extend(this.options, options || {});
         this.className = this.options.className;
 
-        this._window = this._createWindow();
+        if (this.window) {
+            this.destroy();
+        }
+        this.window = this._construct(target);
+    },
+
+    /**
+     * The window constructor
+     *
+     * @param   string  target  ID of the target element
+     * @return  string  XHTML grid
+     */
+    _construct: function (target) {
+        var window = this._createWindow();
+
         if (this.options.modal) {
             this._overlay = this._createOverlay();
         }
+
         if (this.options.showHeader) {
             this._header = this._createHeader();
-        }
-        this._body = this._createBody();
-        if (this.options.closeButton) {
-            this._createClose();
+            window.insert(this._header);
         }
 
-        if (typeof target != 'undefined') {
-            $(target).insert(this._window);
+        this._body = this._createBody();
+        window.insert(this._body);
+
+        if (this.options.closeButton) {
+            window.insert(this._createClose());
+        }
+
+        if (typeof target !== 'undefined') {
+            $(target).insert(window);
         } else {
-            document.body.insert(this._window);
+            document.body.insert(window);
         }
 
         if (this.options.draggable) {
-            if (typeof ProtopackDraggable != 'undefined') {
-                new ProtopackDraggable(this._window,
-                                       this._header? this._header : this._window, 
-                                       {transparent:this.options.transparentDrag});
-            } else if (typeof Draggable != 'undefined') {
+            if (typeof ProtopackDraggable !== 'undefined') {
+                new ProtopackDraggable(window,
+                                       this._header? this._header : window, 
+                                       {transparent: this.options.transparentDrag});
+            } else if (typeof Draggable !== 'undefined') {
+                var options = {};
                 if (!this.options.transparentDrag) {
-                    var options = {starteffect:false, endeffect:false};
+                    options = {starteffect:false, endeffect:false};
                 }
-                new Draggable(this._window, options);
+                new Draggable(window, options);
             }
         }
+
+        return window;
     },
 
     _createWindow: function() {
@@ -92,7 +114,6 @@ var ProtopackWindow = Class.create({
         var header = new Element('div', {'class': this.className + '-header'}),
             title = new Element('div', {'class': this.className + '-title'});
         this._title = title;
-        this._window.insert(header);
 
         return header.insert(title);
     },
@@ -101,7 +122,6 @@ var ProtopackWindow = Class.create({
         var body = new Element('div', {'class': this.className + '-body'}),
             content = new Element('div', {'class': this.className + '-content'});
         this._content = content;
-        this._window.insert(body);
 
         return body.insert(content);
     },
@@ -109,29 +129,30 @@ var ProtopackWindow = Class.create({
     _createClose: function() {
         var btnClose = new Element('span', {'class': this.className + '-close'});
         btnClose.observe('click', this.close.bind(this));
-        this._window.insert(btnClose);
+
+        return btnClose;
     },
 
     _setPosition: function(x, y) {
-        if (typeof x != 'undefined' && typeof y != 'undefined') {
+        if (typeof x !== 'undefined' && typeof y !== 'undefined') {
             try {
-                this._window.style.left = x + 'px';
-                this._window.style.top  = y + 'px';
+                this.window.style.left = x + 'px';
+                this.window.style.top  = y + 'px';
             } catch(err) {
                 throw 'Could not set window position.';
             }
         } else {
             //var dim = document.viewport.getDimensions(),
-            var dim = this._window.parentNode.getDimensions(),
-                width  = this._window.getWidth(),
-                height = this._window.getHeight();
-            this._window.style.left = (width > dim.width)? 0 : Math.round(dim.width / 2 - width / 2) + 'px';
-            this._window.style.top = (height > dim.height)? 0 : Math.round(dim.height / 2 - height / 2) + 'px';
+            var dim = this.window.parentNode.getDimensions(),
+                width  = this.window.getWidth(),
+                height = this.window.getHeight();
+            this.window.style.left = (width > dim.width)? 0 : Math.round(dim.width / 2 - width / 2) + 'px';
+            this.window.style.top = (height > dim.height)? 0 : Math.round(dim.height / 2 - height / 2) + 'px';
         }
     },
 
     setId: function(id) {
-        this._window.id = id;
+        this.window.id = id;
     },
 
     setTitle: function(title) {
@@ -145,26 +166,30 @@ var ProtopackWindow = Class.create({
     },
 
     setSize: function(width, height) {
-        this._window.style.width = width + 'px';
-        this._window.style.height = height + 'px';
+        this.window.style.width = width + 'px';
+        this.window.style.height = height + 'px';
     },
 
     show: function(x, y) {
         this._setPosition(x, y);
-        this._window.show();
-        if (this._overlay) {
+        this.window.show();
+        if (this.options.modal) {
             this._overlay.show();
         }
     },
 
     close: function() {
-        this._window.hide();
+        this.window.hide();
         if (this._overlay) {
             this._overlay.hide();
         }
         if (this.onClose) {
             this.onClose();
         }
+    },
+
+    destroy: function() {
+        this.window.remove();
     }
 
 });
