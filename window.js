2269cf9c7b3705ec7ba6600e9a6e7cbfdb2979fe
Mohsen Khahani <mkhahani@gmail.com>
2012-11-19 14:13:33 +0330

    improve setting position of the window

--- window.js
+++ window.js
@@ -138,10 +138,12 @@ var ProtopackWindow = Class.create({
                 throw 'Could not set window position.';
             }
         } else {
-            //var dim = document.viewport.getDimensions(),
-            var dim = this.window.parentNode.getDimensions(),
+            var dim = (this.window.parentNode === document.body)?
+                    document.viewport.getDimensions() :
+                    this.window.parentNode.getDimensions(),
                 width  = this.window.getWidth(),
                 height = this.window.getHeight();
+            console.log(dim);
             this.window.style.left = (width > dim.width)? 0 : Math.round(dim.width / 2 - width / 2) + 'px';
             this.window.style.top = (height > dim.height)? 0 : Math.round(dim.height / 2 - height / 2) + 'px';
         }
