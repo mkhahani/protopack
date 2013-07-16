45716a3d62ac8e08a37807c618123795ee63f81a
Mohsen Khahani <mkhahani@gmail.com>
2013-07-16 12:21:02 +0430

    remove obsolete methods

--- source/protopack.js
+++ source/protopack.js
@@ -1,13 +1,16 @@
 /**
- *  Protopack is set of DHTML UI Components based on Prototype JS framework
- *  Copyright 2011-2013 Mohsen Khahani
- *  Licensed under the MIT license
- *  http://mohsenkhahani.ir/protopack
+ * Protopack is set of DHTML UI Components based on Prototype JS framework
+ *
+ * @author      Mohsen Khahani <mkhahani@gmail.com>
+ * @copyright   2011-2013 Mohsen Khahani
+ * @license     MIT
+ * @version     1.0
+ * @url         http://mohsenkhahani.ir/protopack
  */
 
 
 /**
- * Checks for existance of the Prototype JS
+ * Checks for existance of Prototype JS
  */
 if (typeof Prototype === 'undefined' || !Prototype.Version.match('1.7')) {
     throw('Protopack requires Prototype JavaScript framework 1.7.0+');
@@ -23,7 +26,7 @@ var Protopack = Protopack || {
 
     /**
      * Adds `observe` and `fire` methods to passed object
-     * Thanks to `Ryan Johnson` for his `LivePipe UI`
+     * Thanks to Ryan Johnson for his LivePipe's Event extension
      */
     extendEvents: function (object) {
         Object.extend(object, {
@@ -67,28 +70,6 @@ Element.addMethods({
         self = $(self);
         return (self.innerText && !window.opera)? self.innerText :
             self.innerHTML.stripScripts().unescapeHTML().replace(/[\n\r\s]+/g, ' ');
-    },
-
-    /**
-     * Sets width of the element to width of the givven element
-     */
-    setWidthTo: function (self, element) {
-        var layout = new Element.Layout(self),
-            sourceWidth = element.getWidth(),
-            padding = layout.get('padding-left') + layout.get('padding-right'),
-            border  = layout.get('border-left')  + layout.get('border-right');
-        self.setStyle({width: sourceWidth - padding - border + 'px'});
-    },
-
-    /**
-     * Sets height of the element to height of the givven element
-     */
-    setHeightTo: function (self, element) {
-        var layout = new Element.Layout(self),
-            sourceHeight = element.getHeight(),
-            padding = layout.get('padding-top') + layout.get('padding-bottom'),
-            border  = layout.get('border-top')  + layout.get('border-bottom');
-        self.setStyle({height: sourceHeight + 'px'});
     }
 });
 
